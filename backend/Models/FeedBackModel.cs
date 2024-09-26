354aabf25bd2abdf2224780609f8ef1bb04fb87e
Will <dinhvanvo510@gmail.com>
2024-09-26 15:03:31 +0700

    dfsd

---
+++ backend/Models/FeedBackModel.cs
@@ -0,0 +1,20 @@
+﻿namespace backend.Models
+{
+    public class FeedBackModel
+    {
+        public FeedBackModel() { }
+        public FeedBackModel(int? id,int userId,int roomId, string? description,DateTime createdAt)
+        {
+            this.Id = id;
+            this.UserId = userId;
+            this.RoomId = roomId;
+            this.Description = description;
+            this.CreatedAt = createdAt;
+        }
+        public int? Id { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ backend/Models/GalleryModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata;
+
+namespace backend.Models
+{
+    public class GalleryModel
+    {
+        public GalleryModel() { }
+        public GalleryModel(int? id, int? room, Blob image)
+        {
+            this.Id = id;
+            this.Room = room;
+            this.Image = image;
+        }
+
+        public int? Id { get; set; }
+        public int? Room { get;  set; }
+        public Blob Image { get;  set; }
+    }
+}
