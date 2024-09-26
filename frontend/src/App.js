c8ed7d2ea08dd7d21351cbfdcb754c71c5870c6f
haohoangnhat782 <haohoangnhat782@gmail.com>
2024-09-26 09:44:59 +0700

    Ui Permission-Coment user() trong App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,84 +1,83 @@
-import React, { useEffect } from "react";
-import { Route, BrowserRouter, Routes, useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
 
-import CustomerLayout from "./Layouts/CustomerLayout";
-import AdminLayout from "./Layouts/AdminLayout";
+import CustomerLayout from './Layouts/CustomerLayout';
+import AdminLayout from './Layouts/AdminLayout';
 
-import CustomerHome from "./components/Customer/Home";
-import Bus from "./components/Customer/Bus";
-import Cart from "./components/Customer/Cart";
-import AdminHome from "./components/Admin/Home";
-import Booking from "./components/Admin/Booking";
-import Contact from "./components/Admin/Contact";
-import Dashboard from "./components/Admin/Dashboard";
-import Discount from "./components/Admin/Discount";
-import Feedback from "./components/Admin/Feedback";
-import Gallery from "./components/Admin/Gallery";
-import Permission from "./components/Admin/Permission";
-import Receipt from "./components/Admin/Receipt";
-import Review from "./components/Admin/Review";
-import Role from "./components/Admin/Role";
-import Room from "./components/Admin/Room";
-import Service from "./components/Admin/Service";
-import Statistic from "./components/Admin/Statistic";
-import User from "./components/Admin/User";
+import CustomerHome from './components/Customer/Home';
+import Bus from './components/Customer/Bus';
+import Cart from './components/Customer/Cart';
+import AdminHome from './components/Admin/Home';
+import Booking from './components/Admin/Booking';
+import Contact from './components/Admin/Contact';
+import Dashboard from './components/Admin/Dashboard';
+import Discount from './components/Admin/Discount';
+import Feedback from './components/Admin/Feedback';
+import Gallery from './components/Admin/Gallery';
+import Permission from './components/Admin/Permission';
+import Receipt from './components/Admin/Receipt';
+import Review from './components/Admin/Review';
+import Role from './components/Admin/Role';
+import Room from './components/Admin/Room';
+import Service from './components/Admin/Service';
+import Statistic from './components/Admin/Statistic';
+import User from './components/Admin/User';
 
-import SignUp from "./components/Authentication/SignUp";
-import SignIn from "./components/Authentication/SignIn";
+import SignUp from './components/Authentication/SignUp';
+import SignIn from './components/Authentication/SignIn';
 
-import { useUser } from "./providers/UserProvider";
+import { useUser } from './providers/UserProvider';
 
 function App() {
-      const { user } = useUser();
-      const isAuthenticated = user !== null;
-      const isCustomer = user?.roleId === 2;
+    // const { user } = useUser();
+    // const isAuthenticated = user !== null;
+    // const isCustomer = user?.roleId === 2;
 
-      return (
-            <BrowserRouter>
-                  <Routes>
-                        {/* Authentication */}
-                        {/* If customer logged in, the customer can not access to the log in or register page */}
-                        {!isAuthenticated && (
-                              <>
-                                    <Route path="/signup" element={<SignUp />} />
-                                    <Route path="/signin" element={<SignIn />} />
-                              </>
-                        )}
-                        {/* If customer logged in with roleId = 2 or default customer does not log in, move on to the customer page. */}
-                        {(isAuthenticated && isCustomer) || !isAuthenticated ? (
-                              // Customer
-                              <Route path="/" element={<CustomerLayout />}>
-                                    <Route index element={<CustomerHome />} />
-                                    {/* <Route path="bus" element={<Bus />} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Authentication */}
+                {/* If customer logged in, the customer can not access to the log in or register page */}
+                {/* {!isAuthenticated && ( */}
+                <>
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/signin" element={<SignIn />} />
+                </>
+                {/* )} */}
+                {/* If customer logged in with roleId = 2 or default customer does not log in, move on to the customer page. */}
+                {/* {(isAuthenticated && isCustomer) || !isAuthenticated ? ( */}
+                {/* // Customer */}
+                <Route path="/" element={<CustomerLayout />}>
+                    <Route index element={<CustomerHome />} />
+                    {/* <Route path="bus" element={<Bus />} />
                                           <Route path="cart" element={<Cart />} /> */}
-                              </Route>
-                        ) : (
-                              // If roleId != 2, move on to the admin page
+                </Route>
+                ) : (
+                {/* // If roleId != 2, move on to the admin page
                               isAuthenticated &&
                               !isCustomer && (
-                                    // Admin
-                                    <Route path="/admin/" element={<AdminLayout />}>
-                                          <Route index element={<AdminHome />} />
-                                          <Route path="booking" element={<Booking />} />
-                                          <Route path="contact" element={<Contact />} />
-                                          <Route path="dashboard" element={<Dashboard />} />
-                                          <Route path="discount" element={<Discount />} />
-                                          <Route path="feedback" element={<Feedback />} />
-                                          <Route path="gallery" element={<Gallery />} />
-                                          <Route path="permission" element={<Permission />} />
-                                          <Route path="receipt" element={<Receipt />} />
-                                          <Route path="review" element={<Review />} />
-                                          <Route path="role" element={<Role />} />
-                                          <Route path="room" element={<Room />} />
-                                          <Route path="service" element={<Service />} />
-                                          <Route path="statistic" element={<Statistic />} />
-                                          <Route path="user" element={<User />} />
-                                    </Route>
-                              )
-                        )}
-                  </Routes>
-            </BrowserRouter>
-      );
+                                    // Admin */}
+                <Route path="/admin/" element={<AdminLayout />}>
+                    <Route index element={<AdminHome />} />
+                    <Route path="booking" element={<Booking />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="discount" element={<Discount />} />
+                    <Route path="feedback" element={<Feedback />} />
+                    <Route path="gallery" element={<Gallery />} />
+                    <Route path="permission" element={<Permission />} />
+                    <Route path="receipt" element={<Receipt />} />
+                    <Route path="review" element={<Review />} />
+                    <Route path="role" element={<Role />} />
+                    <Route path="room" element={<Room />} />
+                    <Route path="service" element={<Service />} />
+                    <Route path="statistic" element={<Statistic />} />
+                    <Route path="user" element={<User />} />
+                </Route>
+                ){/* )} */}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- frontend/src/components/Admin/Permission/index.jsx
+++ frontend/src/components/Admin/Permission/index.jsx
@@ -1,7 +1,175 @@
-import React from "react";
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import DataTable from 'react-data-table-component';
 
 const Permission = () => {
-      return <div></div>;
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const columns = [
+        {
+            name: '#',
+            selector: (row, index) => index + 1,
+        },
+        {
+            name: 'Username',
+            selector: (row) => row.username,
+            sortable: true,
+        },
+        {
+            name: 'Role',
+            selector: (row) => row.role,
+        },
+        {
+            name: 'Status',
+            selector: (row) => (
+                <div className="flex-center">
+                    <button className={`btn ${row.status === 1 ? 'btn-primary' : 'btn-danger'}`} disabled>
+                        {row.status === 1 ? 'Active' : 'InActive'}
+                    </button>
+                </div>
+            ),
+        },
+
+        {
+            name: 'Action',
+            cell: (row) => (
+                <div>
+                    <button
+                        className={`btn ${row.status === 1 ? 'btn-primary' : 'btn-danger'}`}
+                        disabled={row.status === 0}
+                        onClick={handleShow}
+                    >
+                        Permission
+                    </button>
+                </div>
+            ),
+        },
+    ];
+
+    const empdata = [
+        {
+            id: 1,
+            username: 'Madak',
+            role: 'Admin',
+            status: 1,
+        },
+        {
+            id: 2,
+            username: 'Losa',
+            role: 'Admin',
+            status: 1,
+        },
+        {
+            id: 3,
+            username: 'Losv',
+            role: 'Admin',
+            status: 0,
+        },
+    ];
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        // getData();
+        setData(empdata);
+    }, []);
+
+    return (
+        <Fragment>
+            <ToastContainer />
+
+            <br />
+            {/* table */}
+
+            <DataTable title="Premission" columns={columns} data={data} pagination highlightOnHover />
+
+            {/* Modal */}
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title> Permission</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Table bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+
+                                <th>Manage </th>
+                                <th>Fuction </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Manage User</td>
+                                <td>
+                                    <input type="checkbox" id="look"></input>
+                                    &nbsp;
+                                    <label htmlFor="look">Look</label>
+                                    <br></br>
+                                    <input type="checkbox" id="add" />
+                                    &nbsp;
+                                    <label htmlFor="add">Add</label>
+                                    <br></br>
+                                    <input type="checkbox" id="edit" />
+                                    &nbsp;
+                                    <label htmlFor="edit">Edit</label>
+                                    <br></br>
+                                    <input type="checkbox" id="delete" />
+                                    &nbsp;
+                                    <label htmlFor="delete">Delete</label>
+                                    <br></br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Manage Booking</td>
+                                <td>
+                                    <input type="checkbox" id="look"></input>
+                                    &nbsp;
+                                    <label htmlFor="look">Look</label>
+                                    <br></br>
+                                    <input type="checkbox" id="add" />
+                                    &nbsp;
+                                    <label htmlFor="add">Add</label>
+                                    <br></br>
+                                    <input type="checkbox" id="edit" />
+                                    &nbsp;
+                                    <label htmlFor="edit">Edit</label>
+                                    <br></br>
+                                    <input type="checkbox" id="delete" />
+                                    &nbsp;
+                                    <label htmlFor="delete">Delete</label>
+                                    <br></br>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary">Save </Button>
+                </Modal.Footer>
+            </Modal>
+        </Fragment>
+    );
 };
 
 export default Permission;
