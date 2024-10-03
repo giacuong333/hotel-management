13f0bb54ec9e38d035dbed665fa143538bc9503a
Will <dinhvanvo510@gmail.com>
2024-10-03 16:15:59 +0700

    Discount

--- backend/Models/DiscountModel.cs
+++ backend/Models/DiscountModel.cs
@@ -3,15 +3,24 @@
     public class DiscountModel
     {
         public DiscountModel() { }
-        public DiscountModel(int? id, string? name, int value)
+        public DiscountModel(int? id, string? name, float? value, bool? status, DateTime? startAt, DateTime? endAt)
         {
             this.Id = id;
             this.Name = name;
             this.Value = value;
+            this.Status = status;
+            this.StartAt = startAt;
+            this.EndAt = endAt;
         }
 
         public int? Id { get; set; }
         public string? Name { get; set; }
-        public int Value { get; set; }
+        public float? Value { get; set; }
+
+        public bool? Status { get; set; }
+
+        public DateTime? StartAt { get; set; }
+        public DateTime? EndAt{ get; set;}
     }
 }
+
