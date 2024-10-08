b536158d856243b33361259177b6c27d7d7e692e
haohoangnhat782 <haohoangnhat782@gmail.com>
2024-10-08 21:28:39 +0700

    service

--- frontend/src/components/Admin/Service/ServiceForm/index.jsx
+++ frontend/src/components/Admin/Service/ServiceForm/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import { IoClose } from 'react-icons/io5';
 import FormGroup from '~/components/FormGroup';
@@ -8,21 +9,104 @@ import { FaRegUser } from 'react-icons/fa6';
 import { MdOutlineEmail } from 'react-icons/md';
 import { MdLockOutline } from 'react-icons/md';
 import { FiPhone } from 'react-icons/fi';
-
+import ToastContainer, { showToast } from '~/utils/showToast';
 import Button from 'react-bootstrap/Button';
-
-const PopupPanel = ({ data, type, onClose, isShowed }) => {
+import { isEmail, isEmpty, isPhoneNumber, isValidDate, isVerifyPassword } from '~/utils/formValidation';
+const PopupPanel = ({ data, type, onClose, onUserAdded, onUserUpdated, isShowed }) => {
     const [fields, setFields] = useState({
-        name: '',
-        price: '',
-        status: '',
+        name: data?.name || '',
+        price: data?.price || '',
+        status: data?.status || '',
     });
+    const [errors, setErrors] = useState({});
+    // Reset form fields whenever `type` or `data` changes
+    useEffect(() => {
+        setFields({
+            name: data?.name || '',
+            price: data?.price || '',
+            status: data?.status || '',
+        });
+        setErrors({});
+    }, [type, data]);
+
+    const handleValidation = () => {
+        const validationErrors = {};
+
+        if (isEmpty(fields.name)) validationErrors.name = 'Name is required';
+        if (isEmpty(fields.price)) validationErrors.email = 'price is required';
+        if (isEmpty(fields.status)) validationErrors.status = 'Role is required';
+
+        setErrors(validationErrors);
+
+        return Object.keys(validationErrors).length === 0;
+    };
+
+    const handleSubmitClicked = async (e) => {
+        e.preventDefault();
+
+        const apiUrl = 'http://localhost:5058/service';
+
+        if (handleValidation()) {
+            const payload = { ...fields };
+
+            try {
+                if (type === 'add') {
+                    const response = await axios.post(`${apiUrl}/`, payload);
+                    console.log(response);
+                    if (response?.status === 201) {
+                        showToast('Service created successfully', 'success');
+                        setTimeout(handleClose, 4000);
+                        onUserAdded(response?.data?.newService);
+                    }
+                } else if (type === 'edit') {
+                    const response = await axios.put(`${apiUrl}/${data?.id}`, payload);
+                    console.log(response);
+                    if (response?.status === 200) {
+                        showToast(response?.data?.obj?.message, 'success');
+                        setTimeout(handleClose, 4000);
+                        onUserUpdated(response?.data?.obj?.currentService);
+                    }
+                }
+            } catch (error) {
+                if (error?.status === 409) {
+                    showToast(error?.response?.data?.obj?.message, 'error');
+                } else {
+                    showToast(error?.message, 'error');
+                }
+                console.log(error);
+            }
+        }
+    };
+
+    const handleClose = () => {
+        setErrors({});
+        setFields({
+            name: '',
+            price: '',
+            status: '',
+        });
+        onClose();
+    };
 
     const handleInputChanged = () => {};
 
     const handleInputBlured = () => {};
 
     const handleInputTyped = () => {};
+
+    const handleFieldChange = (field, value) => {
+        setFields((prevFields) => ({
+            ...prevFields,
+            [field]: value,
+        }));
+    };
+
+    const handleFieldInput = (field) => {
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            [field]: '',
+        }));
+    };
 
     return (
         <>
@@ -62,30 +146,29 @@ const PopupPanel = ({ data, type, onClose, isShowed }) => {
                             id="name"
                             name="name"
                             type="text"
-                            // error={error}
+                            error={errors.name}
                             Icon={FaRegUser}
-                            value={type !== 'add' ? data?.name : ''}
+                            value={fields?.name}
                             disabled={type === 'see'}
                             customParentInputStyle="p-1 pe-3 rounded-2"
                             customParentParentInputStyle="mt-2"
-                            onChange={handleInputChanged}
-                            onInput={handleInputTyped}
-                            onBlur={handleInputBlured}
+                            onChange={(e) => handleFieldChange('name', e.target.value)}
+                            onInput={() => handleFieldInput('name')}
                         />
                         <FormGroup
                             label="Price:"
                             id="price"
                             name="price"
                             type="text"
-                            // error={error}
+                            error={errors.price}
                             Icon={FaRegUser}
-                            value={type !== 'add' ? data?.price : ''}
+                            // value={type !== 'add' ? data?.price : ''}
+                            value={fields?.price}
                             disabled={type === 'see'}
                             customParentInputStyle="p-1 pe-3 rounded-2"
                             customParentParentInputStyle="mt-2"
-                            onChange={handleInputChanged}
-                            onInput={handleInputTyped}
-                            onBlur={handleInputBlured}
+                            onChange={(e) => handleFieldChange('price', e.target.value)}
+                            onInput={() => handleFieldInput('price')}
                         />
 
                         <FormGroup
@@ -93,18 +176,21 @@ const PopupPanel = ({ data, type, onClose, isShowed }) => {
                             id="status"
                             name="status"
                             type="select"
-                            value={type !== 'add' ? data?.status : ''} // Sử dụng giá trị số 1 hoặc 0
+                            error={errors.status}
+                            // value={type !== 'add' ? data?.status : ''}
+                            value={fields?.status}
                             disabled={type === 'see'}
                             options={[
-                                { label: 'Active', value: 1 },
-                                { label: 'InActive', value: 0 },
+                                { label: '----', value: '' },
+                                { label: 'Active', value: 0 },
+                                { label: 'InActive', value: 1 },
                             ]}
                             customParentInputStyle="p-1 pe-3 rounded-2"
                             customParentParentInputStyle="mt-2"
-                            // onChange={(e) => {
-                            //     // handleFieldChange('status', Number(e.target.value));
-                            //     // handleFieldInput('status');
-                            // }}
+                            onChange={(e) => {
+                                handleFieldChange('status', Number(e.target.value));
+                                handleFieldInput('status');
+                            }}
                         />
                         <FormGroup
                             label="Create time:"
@@ -150,6 +236,7 @@ const PopupPanel = ({ data, type, onClose, isShowed }) => {
                                     type="submit"
                                     variant="primary"
                                     className={`w-full p-2 customer-primary-button`}
+                                    onClick={handleSubmitClicked}
                                 >
                                     Submit
                                 </Button>
