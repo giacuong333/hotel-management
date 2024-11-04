09110a41623faf927e7eac5f063f5d04bebdb863
Will <dinhvanvo510@gmail.com>
2024-11-04 11:11:06 +0700

    .

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9c0e552c13e018b4b4f9e42a80c47505ebf1a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d70bbbf7c83281abc33c2b4a27302724b0b86c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
